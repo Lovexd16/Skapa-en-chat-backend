9244c6f5bc78acfd4114a8b0c30c6524d48f3871
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-08-27 09:27:54 +0200

    Dependency commit
    #1

---
+++ src/main/java/com/skapachatt/skapachatt/SkapachattApplication.java
@@ -0,0 +1,13 @@
+package com.skapachatt.skapachatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkapachattApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkapachattApplication.class, args);
+	}
+
+}
