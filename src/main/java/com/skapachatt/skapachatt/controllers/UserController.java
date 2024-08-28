6b72246088e851a56131294ba5f9437284729374
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-08-28 12:37:29 +0200

    Har lagt till login user endpoint

--- src/main/java/com/skapachatt/skapachatt/controllers/UserController.java
+++ src/main/java/com/skapachatt/skapachatt/controllers/UserController.java
@@ -1,9 +1,12 @@
 package com.skapachatt.skapachatt.controllers;
 
+import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,15 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 import com.skapachatt.skapachatt.models.User;
 import com.skapachatt.skapachatt.services.UserService;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 @CrossOrigin(origins = "*")
 @RestController
 public class UserController {
 
     private UserService userService;
+    private PasswordEncoder passwordEncoder;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
+
+    @Value("${jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpriationMs;
 
     @GetMapping()
     public String getRoot() {
@@ -42,6 +56,29 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Användarnamnet är upptaget.");
         }
+    }
+
+    @PostMapping("/loginuser")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        User existingUser = userService.getUserByUsername(user.getUsername());
+
+        if (existingUser != null) {
+            String encodedPassword = existingUser.getPassword();
+            String incomingPassword = user.getPassword();
+
+            if (passwordEncoder.matches(incomingPassword, encodedPassword)) {
+                System.out.println("inloggad");
+                @SuppressWarnings("deprecation")
+                String token = Jwts.builder()
+                        .setSubject(existingUser.getUsername())
+                        .setIssuedAt(new Date())
+                        .setExpiration(new Date(System.currentTimeMillis() + jwtExpriationMs))
+                        .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                        .compact();
+                return ResponseEntity.ok(token);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Fel användarnamn eller lösenord");
     }
 
 }
